a145671c6761e92a5bd0b6f093339ee819e1f1a8
John P. Masseria <jpmasseria@gmail.com>
2017-03-06 09:56:25 -0500

    Revert "Add `oncrash` to gulpfile to restart after crash" Removed commit doesn't properly call `emit` method.  Need to refactor code more to properly call `emit` method.
    This reverts commit da046e755270069fc68fea0aec46c62049f32d10.

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,5 @@ gulp.task('develop', () => {
   plugins.nodemon({ script: 'bin/www', ext: 'pug js', ignore: ['public/javascripts/**'] })
     .on('restart', () => {
       console.log('restarted!');
-    })
-    .on('crash', () => {
-      console.log('Applicaiton has crashed! Will restart in 10 seconds\n');
-      stream.emit('restart',10); // Restart the server in 10 seconds
     });
 });
